aba651f91792252f7212ea9d803140d28b274647
Mounika <mounikasamudrala9@gmail.com>
2021-04-26 08:21:32 -0700

    Console Application(jenkins example)
    sample Example to test Jenkins

---
+++ Day 19/ConsoleApp(Jenkins Example)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Jenkins_Example_
+{
+   public class Program
+    {
+      public   static void Main(string[] args)
+        {
+        }
+    }
+}
